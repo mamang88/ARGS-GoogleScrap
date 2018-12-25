0b611ffd25b628386d45c99fe03adaf6758cd545
vlausargs <39726092+vlausargs@users.noreply.github.com>
2018-12-25 13:37:13 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -22,6 +22,7 @@ $comment =  "";
 function getKeywordSuggestionsFromGoogle($keyword) {
     $keywords = array();
     echo "masuk ke getkeywords: ".$keyword.'<br>';
+    $keyword=test_input($keyword);
     $data = file_get_contents('http://suggestqueries.google.com/complete/search?output=firefox&client=firefox&hl=en-US&q='.urlencode($keyword));
     if (($data = json_decode($data, true)) !== null) {
         $keywords = $data[1];
