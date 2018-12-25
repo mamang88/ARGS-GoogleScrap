53bac8dff15d43c5cc4d2c5a7d3675450966db9b
vlausargs <39726092+vlausargs@users.noreply.github.com>
2018-12-25 11:51:53 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -50,54 +50,19 @@ if ( isset($_POST["submit"]) ) {
             while( ($row = fgetcsv($fh, 8192)) !== FALSE ) {
               $lines[] = $row;
             }
-            var_dump($lines[0][0]);
+            process_uploadfile($lines);
         }
      } else {
              echo "No file selected <br />";
      }
 }
-if ( $file = fopen($_FILES['file']['tmp_name'] , 'r+' ) ) {
-
-    echo "File opened.<br />";
-
-    $firstline = fgets ($file, 4096 );
-        //Gets the number of fields, in CSV-files the names of the fields are mostly given in the first line
-    $num = strlen($firstline) - strlen(str_replace(";", "", $firstline));
-
-        //save the different fields of the firstline in an array called fields
-    $fields = array();
-    $fields = explode( ";", $firstline, ($num+1) );
-
-    $line = array();
-    $i = 0;
-
-        //CSV: one line is one record and the cells/fields are seperated by ";"
-        //so $dsatz is an two dimensional array saving the records like this: $dsatz[number of record][number of cell]
-    while ( $line[$i] = fgets ($file, 4096) ) {
-
-        $dsatz[$i] = array();
-        $dsatz[$i] = explode( ";", $line[$i], ($num+1) );
-
-        $i++;
+function process_uploadfile($csv){
+  $firstline=array_shift($csv);
+  foreach($csv as $lines){
+    foreach($lines[0] as $keyword){
+      echo $keywords;
     }
-
-        echo "<table>";
-        echo "<tr>";
-    for ( $k = 0; $k != ($num+1); $k++ ) {
-        echo "<td>" . fields[$k] . "</td>";
-    }
-        echo "</tr>";
-
-    foreach ($dsatz as $key => $number) {
-                //new table row for every record
-        echo "<tr>";
-        foreach ($number as $k => $content) {
-                        //new table cell for every field of the record
-            echo "<td>" . $content . "</td>";
-        }
-    }
-
-    echo "</table>";
+  }
 }
 
 function test_input($data) {
