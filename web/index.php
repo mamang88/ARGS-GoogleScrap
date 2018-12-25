1395046ba34e95b2423c9a13e418194b483ea785
vlausargs <39726092+vlausargs@users.noreply.github.com>
2018-12-25 11:04:32 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -65,6 +65,18 @@ foreach ($comment as $line) {
       }
     }; 
   ?></textarea>
+  <form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+<input type="file" name="file">
+<input type="submit" name="btn_submit" value="Upload File" />
+
+<?php
+$fh = fopen($_FILES['file']['tmp_name'], 'r+');
+$lines = array();
+while( ($row = fgetcsv($fh, 8192)) !== FALSE ) {
+  $lines[] = $row;
+}
+var_dump($lines);
+?>
 <h5> Creator : Vlaus </h5>
 <h5>Last Update: 11/7/18 4:30PM GMT+7 </h5>
 </body>
