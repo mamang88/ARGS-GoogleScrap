6f099a4eeb2781bc96acce9ad733913a5a44ecbb
vlausargs <39726092+vlausargs@users.noreply.github.com>
2018-12-25 11:12:03 +0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -28,6 +28,80 @@ function getKeywordSuggestionsFromGoogle($keyword) {
 
     return $keywords;
 }
+if ( isset($_POST["submit"]) ) {
+
+   if ( isset($_FILES["file"])) {
+
+            //if there was an error uploading the file
+        if ($_FILES["file"]["error"] > 0) {
+            echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+
+        }
+        else {
+                 //Print file details
+             echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+             echo "Type: " . $_FILES["file"]["type"] . "<br />";
+             echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+             echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+
+                 //if file already exists
+             if (file_exists("upload/" . $_FILES["file"]["name"])) {
+            echo $_FILES["file"]["name"] . " already exists. ";
+             }
+             else {
+                    //Store file in directory "upload" with the name of "uploaded_file.txt"
+            $storagename = "uploaded_file.txt";
+            move_uploaded_file($_FILES["file"]["tmp_name"], "upload/" . $storagename);
+            echo "Stored in: " . "upload/" . $_FILES["file"]["name"] . "<br />";
+            }
+        }
+     } else {
+             echo "No file selected <br />";
+     }
+}
+if ( $file = fopen( "upload/" . $storagename , r ) ) {
+
+    echo "File opened.<br />";
+
+    $firstline = fgets ($file, 4096 );
+        //Gets the number of fields, in CSV-files the names of the fields are mostly given in the first line
+    $num = strlen($firstline) - strlen(str_replace(";", "", $firstline));
+
+        //save the different fields of the firstline in an array called fields
+    $fields = array();
+    $fields = explode( ";", $firstline, ($num+1) );
+
+    $line = array();
+    $i = 0;
+
+        //CSV: one line is one record and the cells/fields are seperated by ";"
+        //so $dsatz is an two dimensional array saving the records like this: $dsatz[number of record][number of cell]
+    while ( $line[$i] = fgets ($file, 4096) ) {
+
+        $dsatz[$i] = array();
+        $dsatz[$i] = explode( ";", $line[$i], ($num+1) );
+
+        $i++;
+    }
+
+        echo "<table>";
+        echo "<tr>";
+    for ( $k = 0; $k != ($num+1); $k++ ) {
+        echo "<td>" . fields[$k] . "</td>";
+    }
+        echo "</tr>";
+
+    foreach ($dsatz as $key => $number) {
+                //new table row for every record
+        echo "<tr>";
+        foreach ($number as $k => $content) {
+                        //new table cell for every field of the record
+            echo "<td>" . $content . "</td>";
+        }
+    }
+
+    echo "</table>";
+}
 
 function test_input($data) {
   $text = trim($data);
@@ -65,18 +139,21 @@ foreach ($comment as $line) {
       }
     }; 
   ?></textarea>
-  <form action="" method="post" accept-charset="utf-8" enctype="multipart/form-data">
-<input type="file" name="file">
-<input type="submit" name="btn_submit" value="Upload File" />
+<table width="600">
+<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post" enctype="multipart/form-data">
 
-<?php
-$fh = fopen($_FILES['file']['tmp_name'], 'r+');
-$lines = array();
-while( ($row = fgetcsv($fh, 8192)) !== FALSE ) {
-  $lines[] = $row;
-}
-var_dump($lines);
-?>
+<tr>
+<td width="20%">Select file</td>
+<td width="80%"><input type="file" name="file" id="file" /></td>
+</tr>
+
+<tr>
+<td>Submit</td>
+<td><input type="submit" name="submit" /></td>
+</tr>
+
+</form>
+</table>
 <h5> Creator : Vlaus </h5>
 <h5>Last Update: 11/7/18 4:30PM GMT+7 </h5>
 </body>
